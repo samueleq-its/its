fa98f6c71f1b2efd10aa5ab2df21cac88e7194bf
Kerdain96 <samuele.q276@gmail.com>
2026-02-20 10:45:34 +0100

    js regex

--- js html css/JavaScript/querio-samuele-units-04-xx/05b-regex/01-regex-validation/scripts/main.js
+++ js html css/JavaScript/querio-samuele-units-04-xx/05b-regex/01-regex-validation/scripts/main.js
@@ -7,6 +7,7 @@
  * on multiple lines
  */
 
+
 //email address
 let pattern = /\S*@\S*\.[a-zA-Z]{2,4}/;
 let testString = "samuele.querio@edu-its.it";
@@ -52,3 +53,22 @@ testString = "1QqaZ2wsX3edC";
 console.log(`string: "${testString}"
 pattern: ${pattern}
 match: ${testString.match(pattern)}`);
+
+//URL
+pattern = /[a-z][a-z1-9+.-]+:[/]{2}[a-z0-9-.]*\.?[a-zA-Z]{0,4}[/][a-zA-Z0-9_.:/%-]*\?*[a-zA-Z0-9;~&=_.%/-]*#*[a-zA-Z0-9=~:*&,-]*/;
+
+testString = "https://www.youtube.com/watch?v=xC0tctsGquI#t=5";
+console.log(`string: "${testString}"
+pattern: ${pattern}
+match: ${testString.match(pattern)}`);
+
+testString = "https://en.wikipedia.org/wiki/URI_fragment#Examples";
+console.log(`string: "${testString}"
+pattern: ${pattern}
+match: ${testString.match(pattern)}`);
+
+testString = "file:///C:/Users/samue/Documents/ITS/github/its/js%20html%20css/JavaScript/querio-samuele-units-04-xx/05b-regex/01-regex-validation/index.html";
+console.log(`string: "${testString}"
+pattern: ${pattern}
+match: ${testString.match(pattern)}`);
+
