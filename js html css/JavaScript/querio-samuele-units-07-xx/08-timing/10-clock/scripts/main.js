0614e505ca84fc7a0e20c4474c93aa896c1b49d3
samueleq-its <samuele.querio@edu-its.it>
2026-03-06 13:38:25 +0100

    js

--- js html css/JavaScript/querio-samuele-units-07-xx/08-timing/10-clock/scripts/main.js
+++ js html css/JavaScript/querio-samuele-units-07-xx/08-timing/10-clock/scripts/main.js
@@ -1,23 +1,16 @@
 /**
  * @file: main.js
  * @author: samuele.querio@edu-its.it
- * Purpose of file
- *
- * Detailed explanation of what the file does
- * on multiple lines
+ * prints to the console the current time every second in the format HH:MM:SS
  */
 
-/**
- * function description
- * @param {type} paramName description
- * @returns {type} description
- */
 setInterval(
     function () {
         let time = new Date();
-        console.log(`${time.getHours().toString().padStart(2, '0')}:`
+        console.log(
+            `${time.getHours().toString().padStart(2, '0')}:` +
             `${time.getMinutes().toString().padStart(2, '0')}:` +
             `${time.getSeconds().toString().padStart(2, '0')}`
         );},
-        
+        1000
 );
