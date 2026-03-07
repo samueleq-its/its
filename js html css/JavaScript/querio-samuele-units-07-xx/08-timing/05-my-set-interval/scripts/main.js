afd9eb5d289e10c5c15c02a7748838109d1a9a04
Kerdain96 <samuele.q276@gmail.com>
2026-03-07 13:38:20 +0100

    frogs + mySetInterval

--- js html css/JavaScript/querio-samuele-units-07-xx/08-timing/05-my-set-interval/scripts/main.js
+++ js html css/JavaScript/querio-samuele-units-07-xx/08-timing/05-my-set-interval/scripts/main.js
@@ -6,13 +6,14 @@
  */
 
 /**
- * Custom implementation of setInterval, it will fire the function fn every repeatTime milliseconds, 
- * for a total of 15 times.
+ * Custom implementation of setInterval, it will fire the function **fn** every **repeatTime** milliseconds, 
+ * for **repetitions** times (indefinitely if null).
  * @param {Function} fn function to be executed every iteration
  * @param {number} repeatTime time in milliseconds between each execution
+ * @param {null} [repetitions=null] how many times to repeat the execution, if null repeats indefinetly
  * @param  {...any} args arguments to be passed to fn
  */
-function mySetInterval(fn, repeatTime=1000, ...args) {
+function mySetInterval(fn, repeatTime=1000, repetitions=null, ...args) {
     /**
      * Recursive function that will execute fn and then call itself after repeatTime milliseconds, until repetitions reaches 0
      * @param {Function} fn 
@@ -20,26 +21,26 @@ function mySetInterval(fn, repeatTime=1000, ...args) {
      * @param {number} repetitions 
      * @param  {...any} args 
      */
-    function repeater(fn, repeatTime, repetitions = 15, ...args) {
+    function repeater(fn, repeatTime, repetitions, ...args) {
         fn(...args);
-        if (repetitions > 0){
+        if (repetitions == null || repetitions > 1){
             setTimeout(
                 repeater,
                 repeatTime,
-                fn, repeatTime, repetitions-1, ...args
+                fn, repeatTime, repetitions==null ? null : repetitions-1, ...args
             );
         }
     }
 
     setTimeout(
-        repeater(fn, repeatTime, ...args)
+        repeater(fn, repeatTime, repetitions, ...args)
     );
 }
 
 
 let i = 0;
 mySetInterval(
-    (n) => {console.log(i);i++;},1000
+    () => {console.log(i);i++;},1000,15
 )
 
 
