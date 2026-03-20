16103cb604e2de757d0a853b9d4aeda6d0a4511c
samueleq-its <samuele.querio@edu-its.it>
2026-03-20 17:45:27 +0100

    JS enhanced hangman (mancano i commenti)

--- js html css/JavaScript/querio-samuele-units-09-xx/10-events-and-listeners/06-enhanced-word-guesser/scripts/main.js
+++ js html css/JavaScript/querio-samuele-units-09-xx/10-events-and-listeners/06-enhanced-word-guesser/scripts/main.js
@@ -11,75 +11,125 @@ let score = 0;
 let gameOver = false;
 const BONUS = 5;
 const MALUS = -3;
+const outputDiv = document.getElementById("output");
+const detailsDiv = document.getElementById("details");
 
-const targetWord = "hangman";
-let targetLetters = targetWord.split("");
-let guessedLetters = targetWord.replaceAll(/./g, "_").split("");
+let wordsList = [
+	"planet",
+	"library",
+	"javascript",
+	"computer",
+	"mountain",
+	"adventure",
+	"elephant",
+	"notebook",
+	"triangle",
+	"waterfall"
+];
+let targetLetters;
+let discoveredLetters;
+let guessedLetters = [];
 
-/**
- * checks if the character passed as argument is in the word to guess
- * if it is reveal the letter else reduce the number of lives
- * @param {string} letter letter to guess
- */
-function guessLetter(letter) {
-    if (gameOver) {
-        console.log("can't play: GAME OVER");
-        return;
-    }
-    let guessed = false;
-    // loop over the letter of the word to find a match with the parameter
-    for (let i = 0; i < targetLetters.length; i++) {
-        //if the paramenter matches with a letter in the target word, add score, reveal the letter in guessedLetters, and set the flag guessed to true
-        if (targetLetters[i] == letter){
-            // prevent guessing the same letter multplie times
-            if (guessedLetters[i] == letter) {
-                console.log(`${letter} is already guessed`);
-                return;
-            }
-            guessedLetters[i] = letter;
-            score += BONUS;
-            guessed = true;
-        }
-    }
-    // if the letter wasn't found, reduce the score and lives
-    if (!guessed) {
-        lives--;
-        score += MALUS;
-    }
-    // print report to console
-    console.log(
-        `LETTER: ${letter}\n` +
-        `${guessed ? "‼CORRECT‼" : "‼WRONG‼"}\n` +
-        `WORD: ${guessedLetters}\n` +
-        `SCORE: ${score}\n` +
-        `LIVES REMAINING: ${lives}\n`
-    );
-    if  (!guessedLetters.includes("_")) {
-        gameOver = true;
-        console.log("GAME OVER\nYOU'VE WON");
-    } else if (lives <= 0) {
-        gameOver = true;
-        console.log("GAME OVER\nYOU'VE LOST");
-    }
+function setUpGame() {
+	let i = Math.ceil(Math.random() * wordsList.length);
+	let targetWord = wordsList[i];
+	targetLetters = targetWord.split("");
+	discoveredLetters = targetWord.replaceAll(/./g, "_").split("");
 }
 
-guessLetter("g");
-guessLetter("a");
-guessLetter("t");
-guessLetter("o");
-guessLetter("e");
-guessLetter("h");
-guessLetter("b");
-guessLetter("z");
-guessLetter("h");
-guessLetter("m");
-guessLetter("r");
+/**
+ * 
+ * @param {string} letter 
+ * @returns 
+ */
+function guessLetter(letter) {
+	let guessed = false;
+	// loop over the letter of the word to find a match with the parameter
+	for (let i = 0; i < targetLetters.length; i++) {
+		//if the paramenter matches with a letter in the target word, add score, reveal the letter in guessedLetters, and set the flag guessed to true
+		if (targetLetters[i] == letter) {
+			discoveredLetters[i] = letter;
+			score += BONUS;
+			guessed = true;
+		}
+	}
+	// if the letter wasn't found, reduce the score and lives
+	if (!guessed) {
+		lives--;
+		score += MALUS;
+	}
 
+	if (!discoveredLetters.includes("_")) { // win
+		gameOver = true;
+		return "You've Won!";
 
+	} else if (lives <= 0) { // loss
+		gameOver = true;
+		return "You've Lost!";
+	}
+	return guessed ? "Correct!" : "Wrong!";
+}
 
+/**
+ * 
+ */
+function displayDetails() {
 
+	detailsDiv.innerHTML = "";
+	[
+		`Discovered Letters: ${discoveredLetters}`,
+		`Guessed Letters: ${guessedLetters}`,
+		`Score: ${score}`
+	].forEach(
+		line => {
+			let p = document.createElement("p");
+			p.append(line);
+			detailsDiv.append(p);
+		}
+	);
+}
+
+/**
+ * 
+ * @param {string} inputLetter 
+ * @returns 
+ */
+function wordGuesser(inputLetter) {
+	if (gameOver) {
+		outputDiv.textContent = "GAME OVER";
+		return;
+	}
+	//input validation
+	if (inputLetter.length != 1) { // wrong input
+		outputDiv.textContent = "Please insert a single character";
+		return;
+	}
+	if (guessedLetters.includes(inputLetter)) { // already guessed
+		outputDiv.textContent = `'${inputLetter}' was already guessed`;
+		return;
+	}
+	guessedLetters.push(inputLetter); // update guessed letters
+
+	outputDiv.textContent = guessLetter(inputLetter); //progress game and display result
+
+	// update detail text
+	displayDetails();
+
+}
+
+setUpGame();
+displayDetails();
+
+document.getElementById("guess-btn").addEventListener("click",
+	event => {
+		let letterInput = document.getElementById("letter-input");
+		wordGuesser(letterInput.value);
+		letterInput.value = "";
+	}
+);
 
 /* TODO:
-- pick a random word
-- display hanged man lives
+- prevent input of numbers
+- 'enter' without selecting button
+- display full word on game loss
 */
