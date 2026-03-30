5a2ddf1c0d884c5cae573d2abf34d124785d4f58
samueleq-its <samuele.querio@edu-its.it>
2026-03-30 17:48:15 +0200

    03-remote-factory wip

--- Web Dev/JavaScript/11-json-and-ajax/03-remote-factory/scripts/main.js
+++ Web Dev/JavaScript/11-json-and-ajax/03-remote-factory/scripts/main.js
@@ -7,9 +7,38 @@
  * on multiple lines
  */
 
-/**
- * function description
- * @param {type} paramName description
- * @returns {type} description
- */
+const endpoint = "https://api.jsonblob.com/";
+const factoryBlobId = "019d3ebd-7dfa-74ca-92c4-3e3d2dcc2692";
+const carsBlobId = "019d3ebd-d529-7d26-9800-4248845daa43";
 
+let factoryString;
+
+const factoryRequest = new XMLHttpRequest();
+
+factoryRequest.open("GET", endpoint + factoryBlobId);
+// TODO: handle errors
+factoryRequest.onload =
+    () => {
+        // 
+    };
+
+factoryRequest.send();
+
+const carsRequest = new XMLHttpRequest();
+
+carsRequest.open("GET", endpoint + carsBlobId);
+// TODO: handle errors
+carsRequest.onload =
+    () => {
+        // 
+    };
+
+carsRequest.send();
+
+
+
+/*
+TODO:
+    check errors from request
+    blobs might expire
+*/
