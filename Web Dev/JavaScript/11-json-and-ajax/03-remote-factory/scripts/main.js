9f6c2b49d44adcdf6c7a88cd19f30ff18d9e6352
samueleq-its <samuele.querio@edu-its.it>
2026-04-01 17:58:51 +0200

    03-remote-factory wip

--- Web Dev/JavaScript/11-json-and-ajax/03-remote-factory/scripts/main.js
+++ Web Dev/JavaScript/11-json-and-ajax/03-remote-factory/scripts/main.js
@@ -12,55 +12,68 @@ const factoryBlobId = "019d3ebd-7dfa-74ca-92c4-3e3d2dcc2692";
 const carsBlobId = "019d3ebd-d529-7d26-9800-4248845daa43";
 
 function createTextElement(type, text) {
-	let elem = document.createElement(type);
-	elem.textContent = text;
-	return elem
+    let elem = document.createElement(type);
+    elem.textContent = text;
+    return elem
 }
 
 function displayFactory(factoryData) {
-	let factoryDiv = document.getElementById("factory");
-	factoryDiv.innerHTML = "";
-	if (!factoryDiv) { return; }
-	factoryDiv.append(createTextElement("h2", factoryData.name));
-	let text = `${factoryData.address.street}, ${factoryData.address.city}, ${factoryData.address.country}`;
-	factoryDiv.append(createTextElement("p", text));
-	text = `Departments: ${factoryData.departments.join(", ")}`;
-	factoryDiv.append(createTextElement("p", text));
-	text = `Founding year: ${factoryData.foundedYear}`;
-	factoryDiv.append(createTextElement("p", text));
-	if (factoryData.notes) {
-		factoryDiv.append(createTextElement("p", "Notes: " + factoryData.notes));
-	}
+    let factoryDiv = document.getElementById("factory");
+    if (!factoryDiv) { return; }
+    factoryDiv.append(createTextElement("h2", factoryData.name));
+    let text = `${factoryData.address.street}, ${factoryData.address.city}, ${factoryData.address.country}`;
+    factoryDiv.append(createTextElement("p", text));
+    text = `Departments: ${factoryData.departments.join(", ")}`;
+    factoryDiv.append(createTextElement("p", text));
+    text = `Founding year: ${factoryData.foundedYear}`;
+    factoryDiv.append(createTextElement("p", text));
+    if (factoryData.notes) {
+        factoryDiv.append(createTextElement("p", "Notes: " + factoryData.notes));
+    }
 }
+
 //WIP name
 function displayCar(car) {
-	// create HTML
-	let carsDiv = document.getElementById("cars")
-	let list = document.createElement("ul");
-	carsDiv.innerHTML = "";
-	carsDiv.append(list);
-	list.append(createTextElement("li", "Make: " + car.make));
-	list.append(createTextElement("li", "Model: " + car.model));
-	list.append(createTextElement("li", "Year: " + car.year));
-	list.append(createTextElement("li", "Color: " + car.color));
-	list.append(createTextElement("li", "License Plate: " + car.licensePlate));
-	//extra info accordion
-	let accordion = document.createElement("div");
-	accordion.classList.add("accordion");
-	accordion.append(createTextElement("p", "Four wheel drive: " + (car.fourWheelDrive? "yes" : "no")));
-	//	engine
-	accordion.append(createTextElement("h3", "Engine"));
-	// add accordion logic
-	carsDiv.append(accordion);
-	
+    // create HTML
+    const carsDiv = document.getElementById("cars")
+    const carDiv = document.createElement("div");
+    carsDiv.append(carDiv);
+    const list = document.createElement("ul");
+    carDiv.append(list);
+    list.append(createTextElement("li", "Make: " + car.make));
+    list.append(createTextElement("li", "Model: " + car.model));
+    list.append(createTextElement("li", "Year: " + car.year));
+    list.append(createTextElement("li", "Color: " + car.color));
+    list.append(createTextElement("li", "License Plate: " + car.licensePlate));
+    //extra info accordion
+    //make separate function?
+    const accordion = document.createElement("div");
+    carDiv.append(accordion);
+    accordion.append(createTextElement("p", "Four wheel drive: " + (car.fourWheelDrive ? "yes" : "no")));
+    //	engine
+    accordion.append(createTextElement("h4", "Engine"));
+    const accordList = document.createElement("ul");
+    accordion.append(accordList);
+    accordList.append(createTextElement("li", "Type: " + car.engine.type));
+    accordList.append(createTextElement("li", "Displacement: " + car.engine.displacement));
+    accordList.append(createTextElement("li", "Horse power: " + car.engine.horsepower));
+    accordList.append(createTextElement("li", "fuel types: " + car.engine.fuelType.join(", ")));
+    // add accordion logic
+    accordion.classList.add("accordion");
+    carDiv.addEventListener("click",
+        (event) => {
+            accordion.classList.toggle("expanded");
+        }
+    );
+
 }
 
 const factoryRequest = new XMLHttpRequest();
 factoryRequest.open("GET", endpoint + factoryBlobId);
 // TODO: handle errors
 factoryRequest.onload = () => {
-	// display factory info to screen
-	displayFactory(JSON.parse(factoryRequest.responseText));
+    // display factory info to screen
+    displayFactory(JSON.parse(factoryRequest.responseText));
 };
 factoryRequest.send();
 
@@ -68,9 +81,9 @@ const carsRequest = new XMLHttpRequest();
 carsRequest.open("GET", endpoint + carsBlobId);
 // TODO: handle errors
 carsRequest.onload = () => {
-	for (let car of JSON.parse(carsRequest.responseText)) {
-		displayCar(car);
-	}
+    for (let car of JSON.parse(carsRequest.responseText)) {
+        displayCar(car);
+    }
 };
 carsRequest.send();
 
@@ -91,16 +104,16 @@ carsRequest.send();
 
 /*
 TODO:
-	check errors from request
-	blobs might expire
-	loading screen
-	1 listener for every accordion ? 
-	displayFactory uses li instead of p ?
-	ERRORS:
-		blob doesnt exist
-		no/wrong response
+    check errors from request
+    blobs might expire
+    loading screen
+    1 listener for every accordion ? 
+    displayFactory uses li instead of p ?
+    split blobs 1 per car
+    ERRORS:
+        blob doesnt exist
+        no/wrong response
 
 NOTES:
-	jsonblob will expire
-	only used 2 blobs due to jsonblob limitation otherwise 1 json per car
+    jsonblob will expire
 */
