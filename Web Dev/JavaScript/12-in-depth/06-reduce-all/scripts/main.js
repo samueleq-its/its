022cf741c21ef9ea576223f58294a30f127c4a66
Kerdain96 <samuele.q276@gmail.com>
2026-04-19 21:22:53 +0200

    JS 12-in-depth

--- Web Dev/JavaScript/12-in-depth/06-reduce-all/scripts/main.js
+++ Web Dev/JavaScript/12-in-depth/06-reduce-all/scripts/main.js
@@ -1,10 +1,7 @@
 /**
  * @file: main.js
  * @author: samuele.querio@edu-its.it
- * Purpose of file
- *
- * Detailed explanation of what the file does
- * on multiple lines
+ * custom implementations of forEach, map, filter, indexOf and slice using reduce
  */
 
 /**
@@ -67,13 +64,76 @@ function redIndexOf(array, searchElement, fromIndex = 0) {
 
 }
 
+/**
+ * custom implementation of slice using reduce
+ * @param {Array} array array to iterate over
+ * @param {Number} start starting index
+ * @param {Number} end ending index (not included)
+ */
+function redSlice(array, start = 0, end = array.length) {
+	if (start < 0) {
+		start = Math.max(array.length + start, 0);
+	}
+	if (end < 0) {
+		end = Math.max(array.length + end, 0);
+	}
+	return array.reduce(
+		(acc, cur, curIndex) => {
+			if (curIndex >= start && curIndex < end) {
+				acc.push(cur);
+			}
+			return acc;
+		}, []
+	);
+}
 
 
-let a = [1, 2, 3, 4, 2];
-let fn = num => num % 2 == 0;
+let testArrays = [
+	[1, 2, 3, 4, 5],
+	['a', 'b', 'c', 'd', 'e'],
+	[1, 2, 1, 5, 3, 0, 1]
+];
 
-console.log(a.indexOf(2,-0));
-console.log(redIndexOf(a,2,-0));
+console.log('==== Testing Array.foreach() method ====');
+testArrays.forEach(
+	arr => {
+		let output = "";
+		arr.forEach(x => output += x);
+		console.log(output);
+		output = "";
+		redForEach(arr, x => output += x);
+		console.log(output);
+	}
+);
 
+console.log('==== Testing Array.map() method ====');
+testArrays.forEach(
+	arr => {
+		console.log(arr.map(x => x * 2));
+		console.log(redMap(arr, x => x * 2));
+	}
+);
 
+console.log('==== Testing Array.filter() method ====');
+testArrays.forEach(
+	arr => {
+		console.log(arr.filter(x => x % 2 == 0));
+		console.log(redFilter(arr, x => x % 2 == 0));
+	}
+);
 
+console.log('==== Testing Array.indexOf() method ====');
+testArrays.forEach(
+	arr => {
+		console.log(arr.indexOf(1,1));
+		console.log(redIndexOf(arr, 1,1));
+	}
+);
+
+console.log('==== Testing Array.slice() method ====');
+testArrays.forEach(
+	arr => {
+		console.log(arr.slice(1,-1));
+		console.log(redSlice(arr, 1,-1));
+	}
+);
